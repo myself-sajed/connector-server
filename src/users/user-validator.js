8e984b71fcc9b2b44e06eaf9827d9485fd0a7426
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-21 10:28:52 +0530

    add create user validator

---
+++ src/users/user-validator.js
@@ -0,0 +1,14 @@
+import { body } from "express-validator";
+
+export default [
+    body("name").exists().withMessage("A valid name of the user is required"),
+    body("email").exists().withMessage("The email field is missing"),
+    body("bio").exists().withMessage("A valid bio is required"),
+    body("password").exists().withMessage("The password field is missing"),
+    body("avatar").custom((value, { req }) => {
+        if (!req.file) {
+            throw new Error(`Invalid image used in the form`);
+        }
+        return true;
+    }),
+];
