ebc8222f43e6c9015166b1a62aefcd060f6c8055
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-21 10:28:29 +0530

    remove console

--- src/socket/socket-server.js
+++ src/socket/socket-server.js
@@ -1,7 +1,6 @@
 
 function handleSocket(io) {
     io.on("connection", (socket) => {
-        console.log('User Connected', socket.id);
         socket.on('message:client', (message) => {
             console.log('User Message :', message)
             socket.emit("message:server", message)
