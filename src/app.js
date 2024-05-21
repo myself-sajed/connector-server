298d38d3c6503c26e61ae13155005406bcf2504c
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-21 09:53:09 +0530

    add user router

--- src/app.js
+++ src/app.js
@@ -1,36 +1,38 @@
+
+// libraries
 import express from "express";
 import { Server as SocketServer } from 'socket.io'
 import http from 'http'
 import cors from 'cors'
 import config from "./lib/config.js";
-import handleSocket from "./Socket.js";
 
+// routes 
+import userRouter from './users/user-routes.js'
+import handleSocket from "./socket/socket-server.js";
+
+
+// express config
 const app = express();
-
 app.use(cors())
 app.use(express.json());
 
 
-// do the socket connection here
+// socket config
 const server = http.createServer(app);
 const io = new SocketServer(server, {
     cors: {
         origin: config.FRONTEND_URL
     }
 });
-
 handleSocket(io)
 
-app.get("/", (req, res) => {
-    res.send({ status: "success" });
-});
 
 // add routes here
-
+app.use('/users', userRouter)
 
 
 // Global error handling
-app.use((er, req, res, next) => {
+app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     res.status(statusCode).json({
         errors: [
