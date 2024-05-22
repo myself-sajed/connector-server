81828bbcfd638433f04fd7884f4a9b12f9b1c871
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-22 17:54:37 +0530

    find the messages based on contact and me id

--- src/messages/message-controller.js
+++ src/messages/message-controller.js
@@ -3,10 +3,10 @@ import messageService from "./message-service.js"
 const messageController = {
     getMessages: async (req, res) => {
 
-        const { chatId, meId } = req.params
+        const { contactId, meId } = req.params
 
         // get message logic
-        const messages = await messageService.getMessages(chatId, meId)
+        const messages = await messageService.getMessages(contactId, meId)
         res.send(messages || [])
     }
 }
