1ecbf8a6fefe1dbe0da439e40cd806238a95c0e8
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-22 17:55:13 +0530

    remove chatid and add contact id in the get messages endpoint

--- src/messages/message-routes.js
+++ src/messages/message-routes.js
@@ -3,7 +3,7 @@ import messageController from './message-controller.js'
 
 const router = Router()
 
-router.get('/:chatId/:meId', messageController.getMessages)
+router.get('/:contactId/:meId', messageController.getMessages)
 
 
 export default router
