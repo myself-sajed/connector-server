f9198c0aea12e58255d07077e633f19ad645045a
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-22 09:24:10 +0530

    create user socket map for mapping user

--- src/socket/socket-server.js
+++ src/socket/socket-server.js
@@ -1,18 +1,46 @@
-import chatService from "../chats/chat-service.js"
+import chatService from "../chats/chat-service.js";
+
+const userSocketMap = new Map();
 
 function handleSocket(io) {
     io.on("connection", (socket) => {
-        socket.on('message:client', (chatData) => {
-            const { userIds, messageContent, author } = chatData
-            chatService.createOrAppendChat(userIds, messageContent, author)
-            socket.emit("message:server", { message: messageContent, isMe: true })
-        })
+        socket.on('register', (userId) => {
+            userSocketMap.set(userId, socket.id); // Map user ID to socket ID
+            console.log(`User ${userId} connected with socket ID ${socket.id}`);
+        });
+
+        socket.on('message:client', async (chatData) => {
+            const { userIds, messageContent, author } = chatData;
+            const chat = await chatService.createOrAppendChat(userIds, messageContent, author);
+
+            // Get the recipient's user ID
+            const recipientId = userIds.find(id => id !== author);
+
+            // Get the socket IDs of the sender and recipient
+            const senderSocketId = userSocketMap.get(author);
+            const recipientSocketId = userSocketMap.get(recipientId);
+
+            // Emit the message to the sender and recipient
+            if (senderSocketId) {
+                io.to(senderSocketId).emit("message:server", { text: messageContent, author: { _id: author }, chat });
+            }
+
+            if (recipientSocketId) {
+                io.to(recipientSocketId).emit("message:server", { text: messageContent, author: { _id: author }, chat });
+            }
+        });
 
         socket.on("disconnect", async () => {
-            console.log("socket disconnect", socket.id)
-        })
-
-    })
+            // Find the user ID associated with the disconnected socket
+            for (let [userId, socketId] of userSocketMap.entries()) {
+                if (socketId === socket.id) {
+                    userSocketMap.delete(userId);
+                    console.log(`User ${userId} disconnected`);
+                    break;
+                }
+            }
+        });
+    });
 }
 
-export default handleSocket
+export default handleSocket;
