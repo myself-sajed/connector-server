c8b5d8a9077cf307d1634e74794f12c864b0ccf3
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-25 15:33:24 +0530

    get the chat from params and remove the contact and me id

--- src/messages/message-controller.js
+++ src/messages/message-controller.js
@@ -3,10 +3,10 @@ import messageService from "./message-service.js"
 const messageController = {
     getMessages: async (req, res) => {
 
-        const { contactId, meId } = req.params
+        const { chatId } = req.params
 
         // get message logic
-        const messages = await messageService.getMessages(contactId, meId)
+        const messages = await messageService.getMessages(chatId)
         res.send(messages || [])
     }
 }
