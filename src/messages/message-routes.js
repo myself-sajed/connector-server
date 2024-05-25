ec6cde05fba0d3cfa09cb163a764fc99aefc7592
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-25 15:33:40 +0530

    add chat id in the params and remove me id and contact id

--- src/messages/message-routes.js
+++ src/messages/message-routes.js
@@ -3,7 +3,7 @@ import messageController from './message-controller.js'
 
 const router = Router()
 
-router.get('/:contactId/:meId', messageController.getMessages)
+router.get('/:chatId', messageController.getMessages)
 
 
 export default router
