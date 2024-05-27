6e47eae97bc7d17b151eb69fa37a57f2df60c8a4
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-27 13:23:40 +0530

    add edit message functiona

--- src/messages/message-service.js
+++ src/messages/message-service.js
@@ -4,7 +4,6 @@ import Message from "./message-model.js"
 
 const messageService = {
     getMessages: async (chatId) => {
-
         try {
             const messages = await Message.find({ chatId }).lean().populate("author").exec();
             return messages || []
@@ -13,6 +12,21 @@ const messageService = {
             return []
         }
 
+    },
+
+    editMessage: async (messageId, messageContent, chatId) => {
+        const message = await Message.findOneAndUpdate({ _id: messageId }, { text: messageContent, isEdited: true, status: "sent" }, { new: true }).populate("author").exec()
+        const chat = await Chat.findOne({ _id: chatId }).select("-messages")
+
+        let lastMessage = null
+
+        if (chat.lastMessage.toString() === message._id.toString()) {
+            lastMessage = message
+        }
+
+        const userIds = message.interactedUsers.map(userId => userId.toString())
+
+        return { message, lastMessage, userIds, operationalMessage: message }
     }
 }
 
