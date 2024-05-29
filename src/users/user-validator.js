69d163195d0cc205509bf6ded7e57e11817a87bb
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-29 20:17:38 +0530

    add avatar validation

--- src/users/user-validator.js
+++ src/users/user-validator.js
@@ -4,10 +4,5 @@ export default [
     body("name").exists().withMessage("A valid name of the user is required"),
     body("email").exists().withMessage("The email field is missing"),
     body("password").exists().withMessage("The password field is missing"),
-    body("avatar").custom((value, { req }) => {
-        if (!req.file) {
-            throw new Error(`Invalid image used in the form`);
-        }
-        return true;
-    }),
+    body("avatar").exists().withMessage("Avatar is missing"),
 ];
