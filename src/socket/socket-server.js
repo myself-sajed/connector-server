a6d4d7fd1e4458283d69f2d0521289bf71b6dbc1
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-30 19:52:48 +0530

    add the function  to handle coding part

--- src/socket/socket-server.js
+++ src/socket/socket-server.js
@@ -1,9 +1,11 @@
 import handleOnClientMessage from "./client-message-socket.js";
+import handleCodeChange from "./handle-code-change.js";
 import handleUserStatus from "./handle-user-status.js";
 import handleMessageOperations from "./handleMessageOperations.js";
 import { handleSocketDisconnection, registerSocketUser } from "./register-disconnection-socket.js";
 
 const userSocketMap = new Map();
+const userCodeMap = new Map();
 
 function handleSocket(io) {
     io.on("connection", (socket) => {
@@ -11,6 +13,7 @@ function handleSocket(io) {
         handleUserStatus(io, socket, userSocketMap);
         handleOnClientMessage(io, socket, userSocketMap);
         handleMessageOperations(io, socket, userSocketMap);
+        handleCodeChange(io, socket, userSocketMap, userCodeMap);
         handleSocketDisconnection(socket, userSocketMap);
     });
 }
