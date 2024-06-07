03bcb26ee24f5999979b04bbf16e36bd6d9aca64
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-06-07 11:27:34 +0530

    remove clg

--- src/auth/cookie-setter.js
+++ src/auth/cookie-setter.js
@@ -4,8 +4,6 @@ function cookieSetter(res, token) {
     const isProduction = config.NODE_ENV === 'prod';
     const frontEndDomain = config.DOMAIN
 
-    console.log(isProduction, frontEndDomain, config);
-
     res.cookie("userToken", token, {
         maxAge: ((60 * 60 * 1000) * 24) * 7, // 7 days
         httpOnly: true,
